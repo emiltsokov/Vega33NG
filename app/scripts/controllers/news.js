d30782fab0d54a1f38d4630154dacabf22241a30
Emil Tsokov <emil@athlonsofia.com>
2015-04-28 13:44:16 +0300

    build ver 2

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -4,6 +4,8 @@ VEGAWEB
     $http.get('../data/news.json').
     success(function(data, status, headers, config) {
       $scope.news = data;
+      // $scope.news = angular.fromJson(data);
+      // $scope.news = JSON.decode(data);
     }).
     error(function(data, status, headers, config) {
     });
